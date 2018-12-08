67b1a77df4d435996947b7f57d83472f9333bf7c
ivirgille <ivirgille@gmail.com>
2018-12-08 16:08:29 -0500

    Navbar with 4 working links on React

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './CSS/index.css';
 
 import Happy from './components/Happy';
@@ -10,9 +11,32 @@ import Guilty from './components/Guilty';
 class Home extends Component{
   render(){
     return(
-      // your code goes here
+      <Router>
+      <div>
+        <ul>
+        <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Guilty">Guilty</Link>
+          </li>
+          <li>
+            <Link to="/Happy">Happy</Link>
+          </li>
+          <li>
+            <Link to="/Sleepy">Sleepy</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Route exact path="/" />
+        <Route exact path="/Guilty" component={Guilty} />
+        <Route path="/Happy" component={Happy} />
+        <Route path="/Sleepy" component={Sleepy} />
+      </div>
+    </Router>
     )
   }
 }
 
-ReactDOM.render( <Home />, document.getElementById('root') );
+ReactDOM.render( <Home />, document.getElementById('root') );                                                
